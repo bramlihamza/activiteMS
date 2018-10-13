4ed90e5f06b001ff3164fbdf1bbefbd6901eb14f
Hamza BRAMLI <hamza.bramli@tunisiancloud.com>
2018-10-13 15:16:19 +0100

    commit partie 2  trie EndPoint /trie

--- src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
+++ src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
@@ -20,5 +20,8 @@ public interface ProductDao extends JpaRepository<Product, Integer> {
     @Query("SELECT id, nom, prix FROM Product p WHERE p.prix > :prixLimit")
     List<Product>  chercherUnProduitCher(@Param("prixLimit") int prix);
 
+//    @Query("SELECT id, nom, prix, prixAchat FROM Product p ORDER BY nom")
+    List<Product>  findAllByOrderByNomAsc();
+
 
 }
--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -124,5 +124,10 @@ public class ProductController {
 
     }
 
+    @GetMapping(value = "produits/trie")
+    public List<Product> trierProduitsParOrdreAlphabetique() {
+        return productDao.findAllByOrderByNomAsc();
+    }
+
 
 }
